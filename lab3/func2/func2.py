69389ab8bb109ee39d73e092e5c74198c308de67
Arsenkb28 <kudaybergen20007@gmail.com>
2025-02-03 13:48:28 +0500

    new

--- lab3/func2/func2.py
+++ lab3/func2/func2.py
@@ -14,9 +14,20 @@ def sublist_above_5p5():
 # print(sublist_above_5p5())
 
 
-from payload import movies
-def categories(category = "Romance"):
-    l = [i['name'] for i in movies if i['category']==category]
-    return l
 
-# print(categories())
+from payload import movies
+def average_imdb(l):
+    d = [i["imdb"] for i in movies if i["name"] in l]
+    return sum(d)/len(l)
+
+# print(average_imdb(["Bride Wars", "Love", "Colonia", "The Help", "Usual Suspects"]))
+
+from payload import movies
+
+def category_imdb(category):
+    l = [i["imdb"] for i in movies if i["category"] == category]
+    return sum(l)/len(l)
+
+# print(category_imdb('Crime')) #4.0
+# print(category_imdb('Romance')) #6.7
+# print(category_imdb('Thriller')) #5.6
